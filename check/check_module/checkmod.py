19b2c01e5eaa73627d29497c5722475dad2c30ca
Wish <wishyt@proton.me>
2025-09-28 09:50:56 -0500

    add custom url suppot for checkmod

--- check/check_module/checkmod.py
+++ check/check_module/checkmod.py
@@ -2,11 +2,16 @@
 # For use in applications.
 # 2025 Freakybob Team. Licensed under GPL-3.0.
 # (you must declare domain in your app! isExist is designed to show if a URL exists)
+# if you want a custom json url, declare jsonurl in your app aswell
 import urllib.request, json 
-def checkForURL(domain):
+def checkForURL(domain, jsonurl=None):
     global isExist
-    with urllib.request.urlopen("https://freakybob-team.github.io/bobnet/json/sites.json") as url:
-        data = json.loads(url.read().decode())
+    if jsonurl in globals():
+        with urllib.request.urlopen(jsonurl) as url:
+            data = json.loads(url.read().decode())
+    else:
+        with urllib.request.urlopen("https://freakybob-team.github.io/bobnet/json/sites.json") as url:
+            data = json.loads(url.read().decode())
     isExist = None
     if domain in data['sitename']:
         isExist = True
