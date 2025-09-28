f50759b1c36d5b9e4331eb95ff6f5ebea98672dd
Wish <wishyt@proton.me>
2025-09-28 09:38:16 -0500

    add support for externl apps to check

--- check/check.py
+++ check/check.py
@@ -1,10 +1,13 @@
 import urllib.request, json 
 with urllib.request.urlopen("https://freakybob-team.github.io/bobnet/json/sites.json") as url:
     data = json.loads(url.read().decode())
+isExist = None
 print("Check for BobNet.")
 print("Check if a BobNet domain exists!")
 domain = input("What domain would you like to check (ex: hi.bob)? ")
 if domain in data['sitename']:
     print(domain + " exists in BobNet's database!")
+    isExist = True
 else:
     print(domain + " doesn't exist in BobNet's database!")
+    isExist = False
